c02cb6dd5cf1db3f8ec52aabcfe2d8be8b84bdb2
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-07-08 23:12:16 +0700

    canvas layout

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,11 @@
+import Canvas from "./_components/canvax";
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  return <Canvas boardId={params.boardId} />;
+};
+
+export default BoardIdPage;
