d839b87ca8c14da2abe1e1ecfc9b3100ab63c92c
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-07-17 16:32:03 +0700

    select layer

--- app/board/[boardId]/_components/canvax.tsx
+++ app/board/[boardId]/_components/canvax.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Info from "./info";
 import { Participants } from "./participants";
 import Toolbar from "./toolbar";
@@ -18,10 +18,11 @@ import {
   useCanUndo,
   useHistory,
   useMutation,
+  useOthersMapped,
   useStorage,
 } from "@liveblocks/react";
 import { CursorsPresence } from "./cursors-presence";
-import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { connectionIdToColor, pointerEventToCanvasPoint } from "@/lib/utils";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 const MAX_LAYERS = 100;
@@ -106,6 +107,41 @@ const Canvas = ({ boardId }: CanvasProps) => {
     [camera, canvasState, history, insertLayer]
   );
 
+  const selections = useOthersMapped((other) => other.presence.selection);
+
+  const onLayerPointerDown = useMutation(
+    ({ self, setMyPresence }, e: React.PointerEvent, layerId: string) => {
+      if (
+        canvasState.mode === CanvasMode.Pencil ||
+        canvasState.mode === CanvasMode.Inserting
+      ) {
+        return;
+      }
+
+      history.pause();
+      e.stopPropagation();
+      const point = pointerEventToCanvasPoint(e, camera);
+
+      if (!self.presence.selection.includes(layerId)) {
+        setMyPresence({ selection: [layerId] }, { addToHistory: true });
+      }
+      setCanvasState({ mode: CanvasMode.Translating, current: point });
+    },
+    [setCanvasState, camera, history, canvasState.mode]
+  );
+
+  const layerIdsToColorSelection = useMemo(() => {
+    const layerIdsToColorSelection: Record<string, string> = {};
+    for (const user of selections) {
+      const [connectionId, selection] = user;
+
+      for (const layerId of selection) {
+        layerIdsToColorSelection[layerId] = connectionIdToColor(connectionId);
+      }
+    }
+    return layerIdsToColorSelection;
+  }, [selections]);
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
@@ -133,8 +169,8 @@ const Canvas = ({ boardId }: CanvasProps) => {
             <LayerPreview
               key={layerId}
               id={layerId}
-              onLayerPointerDown={() => {}}
-              selectionColor="#000"
+              onLayerPointerDown={onLayerPointerDown}
+              selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
           <CursorsPresence />
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,14 @@
-import { Camera } from "@/types/canvas";
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-const COLORS = [
-  "#DC2626",
-  "#D97706",
-  "#059669",
-  "#7C3AED",
-  "#DB2777"
-]
+import { Camera, Color } from "@/types/canvas";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function connectionIdToColor(connectionId: number) : string {
+export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
-};
+}
 
 export function pointerEventToCanvasPoint(
   e: React.PointerEvent,
@@ -22,6 +16,10 @@ export function pointerEventToCanvasPoint(
 ) {
   return {
     x: Math.round(e.clientX) - camera.x,
-    y: Math.round(e.clientY) - camera.y
-  }
+    y: Math.round(e.clientY) - camera.y,
+  };
+}
+
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
 }
