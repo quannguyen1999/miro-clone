4cbee9cb4ae688bb62ccdbc7bdb72af3d462da67
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-07-17 16:55:48 +0700

    selection box

--- app/board/[boardId]/_components/canvax.tsx
+++ app/board/[boardId]/_components/canvax.tsx
@@ -25,6 +25,7 @@ import { CursorsPresence } from "./cursors-presence";
 import { connectionIdToColor, pointerEventToCanvasPoint } from "@/lib/utils";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
+import { SelectionBox } from "./selection-box";
 const MAX_LAYERS = 100;
 interface CanvasProps {
   boardId: string;
@@ -173,6 +174,7 @@ const Canvas = ({ boardId }: CanvasProps) => {
               selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
+          <SelectionBox onResizeHandlePointerDown={() => {}}/>
           <CursorsPresence />
         </g>
       </svg>
---
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -0,0 +1,90 @@
+import { useSelectionBounds } from "@/hook/use-selection-bounds";
+import { LayerType, Side, XYWH } from "@/types/canvas";
+import { useSelf, useStorage } from "@liveblocks/react";
+import { memo } from "react";
+
+interface SelectionBoxProps {
+  onResizeHandlePointerDown: (corner: Side, initialBounds: XYWH) => void;
+}
+
+const HANDLE_WIDTH = 8;
+
+export const SelectionBox = memo(
+  ({ onResizeHandlePointerDown }: SelectionBoxProps) => {
+    const soleLayerId = useSelf((me) => {
+      me.presence.selection.length === 1 ? me.presence.selection[0] : null;
+    });
+
+    const isShowingHandlers = useStorage(
+      (root) =>
+        soleLayerId && root.layers.get(soleLayerId)?.type !== LayerType.Path
+    );
+
+    const bounds = useSelectionBounds();
+
+    if (!bounds) {
+      return null;
+    }
+
+    return (
+      <>
+        <rect
+          className="fill-transparent stroke-blue-500 stroke-1 pointer-events-none"
+          style={{ transform: `translate(${bounds.x}px, ${bounds.y}px)` }}
+          x={0}
+          y={0}
+          width={bounds.width}
+          height={bounds.height}
+        />
+        {isShowingHandlers && (
+          <>
+            <rect
+              className="fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "ns-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${bounds.x - bounds.width / 2 - HANDLE_WIDTH / 2}px, ${bounds.y - HANDLE_WIDTH / 2}px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation();
+              }}
+            />
+
+            <rect
+              className="fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "ns-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${bounds.x - bounds.width / 2 - HANDLE_WIDTH / 2}px, ${bounds.y - HANDLE_WIDTH / 2}px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation();
+              }}
+            />
+
+            <rect
+              className="fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "ns-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${bounds.x - bounds.width / 2 - HANDLE_WIDTH / 2}px, ${bounds.y - HANDLE_WIDTH / 2}px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation();
+              }}
+            />
+          </>
+        )}
+      </>
+    );
+  }
+);
