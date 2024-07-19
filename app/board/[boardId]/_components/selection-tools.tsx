3cf79ac17f3c1d9b47964fc7d00cd85ffc453bc1
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-07-19 11:04:39 +0700

    layer depth

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -16,7 +16,7 @@ import {
 import { useDeleteLayers } from "@/hook/use-delete-layers";
 import { Hint } from "@/components/ui/hint";
 import { Button } from "@/components/ui/button";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -27,6 +27,41 @@ export const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
 
+    const moveToBack = useMutation((
+      {storage}
+    ) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+      const arr = liveLayerIds.toArray();
+
+      for(let i = 0;i < arr.length; i++){
+        if(selection?.includes(arr[i])){
+          indices.push(i);
+        }
+      }
+
+      for(let i = 0;i<indices.length; i++){
+        liveLayerIds.move(indices[i], i);
+      }
+    }, [selection]);
+
+    const moveToFont = useMutation((
+      {storage}
+    ) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+      const arr = liveLayerIds.toArray();
+
+      for(let i = 0;i < arr.length; i++){
+        if(selection?.includes(arr[i])){
+          indices.push(i);
+        }
+      }
+
+      for(let i = indices.length - 1; i >= 0; i--){
+        liveLayerIds.move(indices[i], arr.length - 1 - (indices.length - 1- i));
+      }
+    }, [selection]);
    
     const setFill = useMutation((
         { storage }: any,
@@ -63,6 +98,18 @@ export const SelectionTools = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label="Bring to front" sideOffset={16}>
+            <Button onClick={moveToFont} variant="board" size="icon">
+              <BringToFront />
+            </Button>
+          </Hint>
+          <Hint label="Send to back" side="bottom" sideOffset={16}>
+            <Button onClick={moveToBack} variant="board" size="icon">
+              <SendToBack />
+            </Button>
+          </Hint>
+        </div>
         <div className="flex items-center border-neutral-200 pl-2 ml-2 border-l">
             <Hint sideOffset={16} label="Delete">
                 <Button variant="board" size="icon" onClick={deleteLayers}>
